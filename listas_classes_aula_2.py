208ea247c9cecae4a0b10139186927bfac80ec77
deiv <deivid652015@gmail.com>
2025-04-11 00:00:01 -0300

    estudos sobre classes

---
+++ listas_classes_aula_2.py
@@ -0,0 +1,15 @@
+class Lista:
+    def __init__ (self,tammax):
+        self.elementosnumero = 2
+        self.dados = [0]*tammax
+
+        def consulta(self, x):
+            for _ in range(self.elementosnumero):
+                return self.dados[_] == x
+
+
+                ...
+...
+l = Lista(8)
+
+print(l)
