daad8967eacafcac8a96e6e212e810d405a5bf3c
deiv <deivid652015@gmail.com>
2025-04-14 23:42:32 -0300

    funcao retira_i criada

--- mini_teste_1.py
+++ mini_teste_1.py
@@ -1,10 +1,22 @@
 '''def retornaMaior(self)
-Se a lista não estiver vazia, retorna True e o maior valor de chave armazenada na lista. Se a lista estiver vazia, retorna False e -1.'''
+
+    Se a lista não estiver vazia, retorna True e o maior valor de chave armazenada na lista. Se a lista estiver vazia, retorna False e -1.
+
+-----------------------------------
+
+ def retira_i(self): 
+    retira o i-ésimo elemento da lista, se houver. Considere que o primeiro elemento da lista possui índice zero e que i < maxtam.
+
+
+
+
+'''
+
 
 class Lista: 
-    def __init__(self):
-        self.lista = [1,2,3,4,5,6,7,8]
-        self.tamanhomax=10
+    def __init__(self, tammax):
+        self.lista = [0]*tammax
+        self.n_elementos = 5
 
     def retornaMaior(self):
 
@@ -13,5 +25,11 @@ class Lista:
             return True, maior
         else:
             return False, -1
+    def retira_i(self, i):
+        if i <= len((self.lista)-1):
+            self.lista.remove(self.lista[i])
+            return True
+        else:
+            return False
 
-    
+...
