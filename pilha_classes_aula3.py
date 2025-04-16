282427ef6db409ab34eaf93760a8c365a6b345ef
deiv <deivid652015@gmail.com>
2025-04-15 23:19:24 -0300

    criação da classe pilha com as funções empilha e desempilha

--- pilha_classes_aula3.py
+++ pilha_classes_aula3.py
@@ -1,12 +1,33 @@
 class Pilha:
-    def __init__(self):
+    def __init__(self,tammax):
+        self.dados = []
+        self.tammax = tammax
         pass
 
     def empilha(self, x):
-        pass
-    def desempilha(self, x):
-        pass 
+        if len(self.dados) <self.tammax:
+            self.dados.append(x)
+            return True
+        else:
+            return False
+    def desempilha(self):
+        if self.dados:
+            last = self.dados.pop()
+            return True, last
     
+        else:
+            return False, -1
+
     '''
         retorna True, valor desempilhado
     '''
+    def printa(self):
+        print(self.dados)
+    
+
+
+pilha = Pilha(5)
+pilha.empilha(2)
+pilha.empilha(3)
+pilha.printa()
+pilha.printa()
