7a491472c1d675fc75a543bdcabbec1d7930a973
deiv <deivid652015@gmail.com>
2025-04-22 23:27:09 -0300

    criação da estrutura filas

---
+++ filas_aula4.py
@@ -0,0 +1,11 @@
+class Filas:
+    def __init__(self, tammax):
+        self.dados=[0]*tammax
+        self.nelems = 0
+        self.primeiro =0
+
+
+    def enfileirar(self):
+        
+
+        pass
